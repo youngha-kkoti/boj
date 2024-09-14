38134726f816b8efb2948bb24d6911298222d682
Youngha Park <youngha.kkoti@gmail.com>
2024-09-15 01:25:04 +0900

    Sep 15

---
+++ step/19/24723.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> n;
+  cout << pow(2, n);
+  return 0;
+}
