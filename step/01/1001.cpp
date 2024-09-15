53a343a744887ade0813ae9552b2e7c8967ecc41
Youngha Park <youngha.kkoti@gmail.com>
2024-09-16 02:59:15 +0900

    Sep 16

---
+++ step/01/1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> a >> b;
+  cout << a - b;
+  return 0;
+}
