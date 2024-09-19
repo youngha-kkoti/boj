83bb9efed3c79f713fe95bc35b99e0d216fc1b91
Youngha Park <youngha.kkoti@gmail.com>
2024-09-19 11:45:18 +0900

    01 (complete)

---
+++ step/01 (complete)/10172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cout << "|\\_/|" << "\n";
+  cout << "|q p|   /}" << "\n";
+  cout << "( 0 )\"\"\"\\" << "\n";
+  cout << "|\"^\"`    |" << "\n";
+  cout << "||_/=\\\\__|" << "\n";
+  return 0;
+}
